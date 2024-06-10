4a0eab30b1c63c021756bf7a89ea6750af1b11ec
kevinDodiya <141211573+kevinDodiya@users.noreply.github.com>
2024-06-10 12:30:32 +0530

    Revert "Dev kevin"

--- Server/src/routes/launch/launch.router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getAll } = require('./launch.controller');
-
-const launchRouter = express.Router();
-
-launchRouter.get('/getall', getAll); // Correct path
-
-module.exports = launchRouter;
