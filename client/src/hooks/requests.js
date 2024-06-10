a421d1508629394973d0374c580d756bb81f4e29
sahil <sahil@truesys.co.in>
2024-06-10 15:48:29 +0530

    make launche static data api working

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -37,9 +37,13 @@ httpGetPlanets().then(planets => console.log(planets)).catch(error => console.er
 
 
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const response = await fetch(`${apiController}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
+
 
 async function httpSubmitLaunch(launch) {
   // TODO: Once API is ready.
