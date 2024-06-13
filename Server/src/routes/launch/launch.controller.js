bc6477b0ac280280480f28f0c63d289a8064bc83
kevinDodiya <kevindodiya75@gmail.com>
2024-06-13 11:49:41 +0530

    Rebuild again

--- Server/src/routes/launch/launch.controller.js
+++
@@ -1,7 +0,0 @@
-const { launches } = require('../../models/launches.model');
-
-function getAll(req, res) {
-  return res.status(200).json(Array.from(launches.values()));
-}
-
-module.exports = { getAll };
